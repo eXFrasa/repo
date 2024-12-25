e51775f8eac314fb16ea0ad54579a6bc1346791a
Firmansyah <64854180+firmansyahbio@users.noreply.github.com>
2024-12-26 00:40:34 +0700

    Create script.js

---
+++ docs/js/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+	function toggle_click_sidebar() {
+	// Klik pada tombol atau link tautan
+	    $(".toggle-click-sidebar").on("click", function(e){
+	      $("body").addClass("show-sidebar noscroll");
+      });
+
+	  // Tutup atau Hapus Bars
+      function close_click_sidebar() {
+        // Sembunyikan kembali modal Bars
+        $("body").removeClass("show-sidebar noscroll");
+}
+
+      // Jika tombol/elemen 'tutup', atau overlay diklik
+	    $('body').on('click', '.close-label, .amp-sidebar-container + .sidebar-overlay', function(event) {
+          // Hibungi tutup dan
+          close_click_sidebar();
+      });
+      // Jika tombol ESC diketuk
+      $('body').keyup(function(e) {
+          // Kunci ESC dipetakan ke kode kunci `27`
+          if (e.keyCode == 27) { 
+            // Panggil fungsi tutup dan setel ulang
+            close_click_sidebar();
+          }
+      });
+	}
+	toggle_click_sidebar();
+    
+    
+    function toggle_click_archive() {
+	// Klik pada tombol atau link tautan
+	    $(".toggle-click-archive").on("click", function(e){
+	      $("body").addClass("show-archive noscroll");
+      });
+
+	  // Tutup atau Hapus Bars
+      function close_click_archive() {
+        // Sembunyikan kembali modal Bars
+        $("body").removeClass("show-archive noscroll");
+}
+
+      // Jika tombol/elemen 'tutup', atau overlay diklik
+	    $('body').on('click', '.close-archive, .container-modal-archive .overlay', function(event) {
+          // Hibungi tutup dan
+          close_click_archive();
+      });
+      // Jika tombol ESC diketuk
+      $('body').keyup(function(e) {
+          // Kunci ESC dipetakan ke kode kunci `27`
+          if (e.keyCode == 27) { 
+            // Panggil fungsi tutup dan setel ulang
+            close_click_archive();
+          }
+      });
+	}
+	toggle_click_archive();
+  });
